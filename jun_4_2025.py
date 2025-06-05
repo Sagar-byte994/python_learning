ad8b7172c4cfe446f5d56623c7142318d2b89544
sagar <innocentsagar20@gmail.com>
2025-06-05 10:40:10 +0530

    added jun-2-2025 learnig file update git ignore file added jun-3-2025 learning file

---
+++ jun_4_2025.py
@@ -0,0 +1,28 @@
+# tup = (2, 1, 3, 1)
+# print(tup[0])
+# print(tup[1])
+# tup[0] = 5
+
+# tup = (1,)
+# print(tup)
+# print(type(tup))
+
+# tup = (1, 2, 3, 4)
+# print(tup[1:4])
+
+# tup = (1, 2, 3, 4)
+# print(tup.index(2))
+
+# tup = (1, 2, 3, 2, 4, 2)
+# print(tup.count(2))
+
+movies = ()
+mov1 = input("enter 1st movie: ")
+mov2 = input("enter 2nd movie: ")
+mov3 = input("enter 3rd movie: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
