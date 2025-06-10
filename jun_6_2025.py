6a16e3a8d2f815c801c44a16f084b37d22e92549
sagar <innocentsagar20@gmail.com>
2025-06-10 12:02:24 +0530

    added jun-2-2025 learnig file update git ignore file added jun-3-2025 learning file

--- jun_6_2025.py
+++ jun_6_2025.py
@@ -1,9 +1,17 @@
+# list1 = [1, 2, 3, 2, 1]
+# list2 = [1, 2, 3, 4, 5]
+#
+# copy_list2 = list2.copy()
+# copy_list2.reverse()
+#
+# if(copy_list2 == list2):
+#     print("palindrome")
+# else:
+#     print("NOT palindrome")
 
-list1 = [1, 2, 3, 2, 1]
-list2 = [1, 2, 3, 4, 5]
-copy_list1 = list.copy()
-copy_list1.reverse()
-if(copy_list1 == list1):
-    print("palindrome")
-else:
-    print("NOT palindrome")
+# student_grade =["C", "D", "A", "A", "B", "B", "A"]
+# print(student_grade.count("A"))
+
+# student_grade =["C", "D", "A", "A", "B", "B", "A"]
+# student_grade.sort()
+# print(student_grade)
