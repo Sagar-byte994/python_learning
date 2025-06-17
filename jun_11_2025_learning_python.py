2096f95d1746dd12b001fa149b284ec58ab88878
sagar <innocentsagar20@gmail.com>
2025-06-17 10:31:28 +0530

    added jun-2-2025 learnig file update git ignore file added jun-3-2025 learning file

---
+++ jun_11_2025_learning_python.py
@@ -0,0 +1,11 @@
+info = {
+    "name" : "sagar",
+    "subject" : ["python", "c", "java"],
+    "topics" : ("dict", "set"),
+    "learning" : "coding",
+    "age" : 25,
+    "is adult" : True,
+    "marks" : 94.4
+}
+
+print(info)
