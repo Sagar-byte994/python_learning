2ca2252812d7e4213b2cbfcd5b588f67644d3626
sagar <innocentsagar20@gmail.com>
2025-06-18 12:41:19 +0530

    Dictionary And Dictionary Methods

---
+++ jun_18_2025_learning_ python.py
@@ -0,0 +1,19 @@
+# student = {
+#     "name" : "sagar",
+#     "subjects" : {
+#     "phy" : 97,
+#     "chem" : 98,
+#     "math" : 95,
+#     }
+# }
+# new_dict = {"city": "delhi","age": 16}
+# student.update(new_dict)
+# print(student)
+
+# collection = {1, 2, 2, 2,"hello", "world", "world", 4}
+# print(collection)
+# print(type(collection))
+# print(len(collection))
+
+# collection = set()
+# print(type(collection))
