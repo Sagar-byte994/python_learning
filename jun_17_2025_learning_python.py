b90d8d318e494c4ab7d0f01e6ba057d05ac3b8bc
sagar <innocentsagar20@gmail.com>
2025-06-18 10:15:55 +0530

    Dictionary And Dictionary Methods

---
+++ jun_17_2025_learning_python.py
@@ -0,0 +1,95 @@
+# info = {
+#     "key" : "value",
+#     "name" : "apnacollage",
+#     "learning" : "codding",
+#     "age" : 35,
+#     "is_adult" : True,
+#     "marks" : 9.4
+# }
+# print(info)
+
+# info = {
+#     "name" : "sagar",
+#     "subjects" : ["python", "c", "java"],
+#     "topics" : ("dict", "set"),
+#     "age" : 25,
+#     "is_adult" : True,
+#     "marks"  :94.4
+# }
+#
+# info["name"] = "rajat"
+# info["surname"] = "bhasin"
+# print(info)
+
+# info = {
+#     "name" : "sagar",
+#     "subjects" : ["python", "c", "java"],
+#     "topics" : ("dict", "set"),
+#     "age" : 25,
+#     "is_adult" : True,
+#     "marks"  :94.4
+# }
+# null_dict = {}
+# null_dict["name"] =  "apnacollage"
+# print(null_dict)
+
+# student = {
+#     "name" : "sagar",
+#     "subject" : {
+#     "phy" : 97,
+#     "chem" : 98,
+#     "math" : 95,
+#     }
+# }
+# print(len(student))
+# print(list(student.keys()))
+
+# student = {
+#     "name" : "sagar",
+#     "subjects" : {
+#     "phy" : 97,
+#     "chem" : 98,
+#     "math" : 95,
+#     }
+# }
+# print(student.keys())
+
+# student = {
+#     "name" : "sagar",
+#     "subjects" : {
+#     "phy" : 97,
+#     "chem" : 98,
+#     "math" : 95,
+#     }
+# }
+# print(student.values())
+
+# student = {
+#     "name" : "sagar",
+#     "subjects" : {
+#     "phy" : 97,
+#     "chem" : 98,
+#     "math" : 95,
+#     }
+# }
+# print(student.items())
+
+# student = {
+#     "name" : "sagar",
+#     "subjects" : {
+#     "phy" : 97,
+#     "chem" : 98,
+#     "math" : 95,
+#     }
+# }
+# print(list(student.values()
+
+student = {
+    "name" : "sagar",
+    "subjects" : {
+    "phy" : 97,
+    "chem" : 98,
+    "math" : 95,
+    }
+}
+print(student.get("name2"))
