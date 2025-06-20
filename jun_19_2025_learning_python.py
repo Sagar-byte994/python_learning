5185246363dc6ab67c139c3f2c9a107697286bdc
sagar <innocentsagar20@gmail.com>
2025-06-20 10:58:09 +0530

    Dictionary And Dictionary Methods

---
+++ jun_19_2025_learning_python.py
@@ -0,0 +1,25 @@
+# subject = {
+#     "python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"}
+#
+# print(len(subject))
+
+# marks = {}
+# x = int = (input("enter phy :  "))
+# marks.update(({"phy" : x}))
+# x = int = (input("enter math :  "))
+# marks.update(({"math" : x}))
+# x = int = (input("enter ches :  "))
+# marks.update(({"ches" : x}))
+# print(marks)
+
+
+# values = {
+#     ("float", 9.0),
+#     ("int", 9)
+# }
+# print(values)
+#
+
+# values = {"9", "9.0"}
+# print(values)
+
