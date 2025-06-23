f24969cb4a2592fc31b2ecc469b7f894a408f7c4
sagar <innocentsagar20@gmail.com>
2025-06-23 10:57:56 +0530

    Dictionary And Dictionary Methods

---
+++ jun_20_2025_learning_python.py
@@ -0,0 +1,22 @@
+# count = 1
+# while count <= 5:
+#     print("hello")
+#     count += 1
+
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+# print("loop ended")
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i +=1
+
