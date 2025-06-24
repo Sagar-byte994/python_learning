a975bd627c3d4e402fa44f45433b95858e33a710
sagar <innocentsagar20@gmail.com>
2025-06-24 11:29:07 +0530

    Dictionary And Dictionary Methods

---
+++ jun_23_2025_learning_python.py
@@ -0,0 +1,89 @@
+# n = int(input("enter number :"))
+# i = 1
+# while i <= 10:
+#     print(n*i)
+#     i +=1
+
+# nums =[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1
+
+# heroes = ["ironman", "batman", "thor", "superman", "hunk"]
+# i = 0
+# while i < len(heroes):
+#     print(heroes[i])
+#     i += 1
+
+# n = int(input("enter number :"))
+# nums =[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+#
+#
+# i = 0
+# while i < len(nums):
+#     if(nums[i] == n):
+#      print("found at idx", i)
+#     else:
+#      print("finding")
+#     i += 1
+
+
+# nums =[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+#
+# x = 36
+# i = 0
+# while i < len(nums):
+#     if(nums[i] == x):
+#      print("found at idx", i)
+#      break
+#     else:
+#      print("finding")
+#     i += 1
+#
+# print("end of loop")
+
+# i = 1
+# while i <= 10:
+#     if(i%2 == 0):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# i = 1
+# while i <= 10:
+#     if(i%2 != 0):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+# veggies = ["potato", "cucumber", "brinjal", "ladyfinger"]
+#
+# for val in veggies:
+#     print(val)
+
+# tup = (1, 2, 3, 4, 2, 6, 7, 8)
+# for num in tup:
+#     print(num)
+
+# str = "apnacollage"
+#
+# for char in str:
+#     print(char)
+
+# num = [1, 4, 16, 25, 36, 49, 64, 81, 100]
+#
+# for el in num:
+#     print(el)
+
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 64)
+x = 64
+
+idx = 1
+for el in nums:
+    if(el == x):
+        print("number found at idx", idx)
+        break
+idx += 1
