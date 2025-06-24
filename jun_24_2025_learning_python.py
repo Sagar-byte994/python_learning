71f8da8ebc8b4dc4fc5a9cdcdf54cc061181c834
sagar <innocentsagar20@gmail.com>
2025-06-24 16:09:29 +0530

    Dictionary And Dictionary Methods

---
+++ jun_24_2025_learning_python.py
@@ -0,0 +1,33 @@
+# for i in range(10):
+#     print(i)
+
+# for i in range(2, 10):
+#     print(i)
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+# for i in range(1, 10, 2 ):
+#      print(i)
+
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# for el in list:
+#      print(el)
+
+# for i in range(100, 0, -1):
+#      print(i)
+
+# n = int(input( "enter a number :"))
+# for i in range(1 , 11):
+#      print(n * i)
+
+# for i in range (5):
+#      pass
+#
+# if i > 5:
+#      pass
+# print("some useful work")
+
+n = 5
+
+fori in range
