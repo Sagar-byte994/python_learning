562cb54cc6ef8ea87dcdeecbca6d38b5ff5547cb
sagar <innocentsagar20@gmail.com>
2025-06-26 10:56:29 +0530

    Dictionary And Dictionary Methods

---
+++ jun_25_2025_learning_python.py
@@ -0,0 +1,34 @@
+# n = 7
+#
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+#     print("total sum =", sum)
+
+# n = 7
+# fact = 1
+#
+# for i in range(1, n+1):
+#     fact *= i
+#     print("Factorial =", fact)
+
+# n = 5
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= i
+#     i += 1
+#     print("Factorial =", fact)
+
+# def calc_sum (a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+# calc_sum(5, 10)
+#
+# calc_sum(20,50)
+
+# def calc_sum(a, b):
+#     return a + b
+# sum = calc_sum(805, 195)
+# print(sum)
