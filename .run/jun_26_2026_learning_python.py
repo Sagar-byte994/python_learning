32d7f909e0a2546e4d330cab52535c3ad51fa6ea
sagar <innocentsagar20@gmail.com>
2025-06-27 10:26:05 +0530

    Dictionary And Dictionary Methods

---
+++ .run/jun_26_2026_learning_python.py
@@ -0,0 +1,42 @@
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg(98, 92, 93)
+
+# print("apnacollage", end=" ")
+# print("sagar")
+
+# def cal_prod(a, b= 10):
+#     print(a * b)
+#     return a * b
+# cal_prod(10)
+
+# cities = ["delhi", "gurgaon", "noida", "pune", "mumbai", "chennai"]
+# heroes = ["thor", "batman", "spiderman", "hunk"]
+# def print_len(list):
+#     print(len(list))
+#
+# print_len(cities)
+# print_len(heroes)
+
+# cities = ["delhi", "gurgaon", "noida", "pune", "mumbai", "chennai"]
+# heroes = ["thor", "batman", "spiderman", "hunk"]
+#
+# def print_len(list):
+#     print_len(list)
+#
+# def print_list(list):
+#     for item in list:
+#         print(item, end=" ")
+
+# print(cities)
+# n = 5
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+# cal_fact(n)
