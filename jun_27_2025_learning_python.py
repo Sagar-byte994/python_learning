16aa395ed7dac01bebf60f59c6415eb648459afa
sagar <innocentsagar20@gmail.com>
2025-06-30 11:01:39 +0530

    Dictionary And Dictionary Methods

---
+++ jun_27_2025_learning_python.py
@@ -0,0 +1,24 @@
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+# cal_fact(5)
+
+# def converter(usd_val):
+#     inr_val = usd_val * 83
+#     print(usd_val, "usd =", inr_val, "inr")
+# converter(10)
+
+# def show(n):
+#     if(n ==-1) :
+#         return
+#     print(n)
+#     show(n-1)
+# show(5)
+
+# def fact(n):
+#     if(n ==1 or n == 0):
+#          return 1
+#     return fact(n-1) * n
+# print(fact(5))
