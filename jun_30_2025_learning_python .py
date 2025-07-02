290f5e9f4eb8871b02ac020e67a39037fb0c9294
sagar <innocentsagar20@gmail.com>
2025-07-02 10:35:49 +0530

    Dictionary And Dictionary Methods

---
+++ jun_30_2025_learning_python .py
@@ -0,0 +1,15 @@
+# def calc_sum(n):
+#     if(n == 0):
+#          return 0
+#     return calc_sum(n-1) + n
+#
+# sum = calc_sum(10)
+# print(sum)
+
+# def print_list(list, idx=0):
+#     if(idx == len(list)):
+#         return
+#     print(list[idx])
+#     print_list(list, idx+1)
+# fruits = ["mango", "apple", "banana","lichi", "grapes"]
+# print_list(fruits)
