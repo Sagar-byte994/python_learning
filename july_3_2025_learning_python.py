c447fcf57930d2f8352cdbdc56809a7bca8e5f80
sagar <innocentsagar20@gmail.com>
2025-07-04 11:12:54 +0530

    generate qr code

---
+++ july_3_2025_learning_python.py
@@ -0,0 +1,12 @@
+# f = open("/home/sagar/Desktop/application/projects/python_learning/abc.txt", "r")
+# data = f.read()
+# print(data)
+# print(type(data))
+# f.close()
+
+# f = open("/home/sagar/Desktop/application/projects/python_learning/abc.txt", "r")
+# data = f.read(4)
+# print(data)
+# print(type(data))
+# f.close()
+
